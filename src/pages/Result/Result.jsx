ba6f8cbe8a237fbcd2b6947ddcb073dcd3d9f8ef
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-08-19 11:10:33 +0600

    result page created

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -4,11 +4,40 @@ const Result = () => {
     const location = useLocation();
     const allInputData = location.state;
 
-    console.log(allInputData);
-
     return (
-        <div>
-            result
+        <div className="mt-10">
+            <div className="overflow-x-auto">
+                <table className="table table-sm md:table-md lg:table-lg">
+                    <thead>
+                        <tr>
+                            <th>Project Name</th>
+                            <th>Description</th>
+                            <th>Client</th>
+                            <th>Contractor</th>
+                            <th>Max_X</th>
+                            <th>Min_X</th>
+                            <th>Max_Y</th>
+                            <th>Min_Y</th>
+                            <th>Max_Z</th>
+                            <th>Min_Z</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{allInputData.projectName}</td>
+                            <td>{allInputData.description}</td>
+                            <td>{allInputData.client}</td>
+                            <td>{allInputData.contractor}</td>
+                            <td>{allInputData.max_X}</td>
+                            <td>{allInputData.min_X}</td>
+                            <td>{allInputData.max_Y}</td>
+                            <td>{allInputData.min_Y}</td>
+                            <td>{allInputData.max_Z}</td>
+                            <td>{allInputData.min_Z}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
